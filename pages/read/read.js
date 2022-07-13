81e658d36514bb9b3e8620a9f1b0014514485b44
Byron de Villiers <Byron.DeVilliers@capetown.gov.za>
2022-07-13 16:35:52 +0200

    clean up code, added navigation parameters, updated the read page and the home page to use the DOM to show blog posts

--- pages/read/read.js
+++ pages/read/read.js
@@ -5,8 +5,9 @@ console.log(params);
 const blogPosts = [
   {
     id: 1,
-    title: "blog 1",
-    description: "New Blog post",
+    title: "Blog 1",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     author: " Byron",
     dateCreated: new Date(),
     imgURL:
@@ -31,8 +32,9 @@ const blogPosts = [
   },
   {
     id: 2,
-    title: "blog 2",
-    description: "New Blog post",
+    title: "Blog 2",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     author: " Byron",
     dateCreated: new Date(),
     imgURL:
@@ -57,8 +59,9 @@ const blogPosts = [
   },
   {
     id: 3,
-    title: "blog 3",
-    description: "New Blog post",
+    title: "Blog 3",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     author: " Byron",
     dateCreated: new Date(),
     imgURL:
@@ -83,5 +86,37 @@ const blogPosts = [
   },
 ];
 
-const blog = blogPosts.find(x => x.id == Number (params.blogID))
-console.log(blog)
+const blog = blogPosts.find((x) => x.id == Number(params.blogID));
+console.log(blog);
+
+const title = document.getElementById("title");
+title.style.marginBottom = "16px";
+title.innerText = blog.title;
+
+const description = document.getElementById("description");
+description.style.marginBottom = "16px";
+description.innerText = blog.description;
+
+const metaData = document.getElementById("meta-data");
+metaData.innerText = `Published by ${
+  blog.author
+} on ${blog.dateCreated.toDateString()}`;
+
+const content = document.getElementById("content");
+
+for (let i = 0; i < blog.content.length; i++) {
+  const section = blog.content[i];
+
+  let div = document.createElement("div");
+  div.style.marginBottom = "16px";
+  let h3 = document.createElement("h3");
+  let p = document.createElement("p");
+  let headerTextNode = document.createTextNode(section.sectionHeader);
+  let sectionTextNode = document.createTextNode(section.sectionText);
+
+  h3.appendChild(headerTextNode);
+  p.appendChild(sectionTextNode);
+  div.appendChild(h3);
+  div.appendChild(p);
+  content.appendChild(div);
+}
